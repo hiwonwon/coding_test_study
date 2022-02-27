2d8f5ba80af34677cf0c0d6a457b6bd4e12cc7cd
HYEWON PARK <hyewon9913@gmail.com>
2022-02-28 00:01:48 +0900

    study6_9,10

--- coding_test_study/src/coding_test_study_phw/study_chapter6_9.java
+++ coding_test_study/src/coding_test_study_phw/study_chapter6_9.java
@@ -1,5 +1,42 @@
 package coding_test_study_phw;
 
-public class study_chapter6_9 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class study_chapter6_9 {
+	public int count(int[] arr, int capacity){
+		int cnt=1, sum=0;
+		for(int x : arr){
+			if(sum+x>capacity){
+				cnt++;
+				sum=x;
+			}
+			else sum+=x;
+		}
+		return cnt;
+	}
+
+	public int solution(int n, int m, int[] arr){
+		int answer=0;
+		int lt=Arrays.stream(arr).max().getAsInt();
+		int rt=Arrays.stream(arr).sum();
+		while(lt<=rt){
+			int mid=(lt+rt)/2;
+			if(count(arr, mid)<=m){
+				answer=mid;
+				rt=mid-1;
+			}
+			else lt=mid+1;
+		}
+		return answer;
+	} 
+	public static void main(String[] args){
+		study_chapter6_9 T = new study_chapter6_9();
+		Scanner kb = new Scanner(System.in);
+		int n=kb.nextInt();
+		int m=kb.nextInt();
+		int[] arr=new int[n];
+		for(int i=0; i<n; i++) arr[i]=kb.nextInt();
+		System.out.print(T.solution(n,m,arr));
+	}
 }
